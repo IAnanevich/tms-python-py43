4b297317fa2f9302120abbae22fd6fc114d16485
Eugenia Demianchuk <mega20031707@gmail.com>
2023-08-07 00:47:48 +0300

    fixed HW-3

--- src/eugenia_hw3/eugenia_hw3_4.py
+++ src/eugenia_hw3/eugenia_hw3_4.py
@@ -1,15 +1,14 @@
-""" first variant
+
 
 x = int(input())
 sum_1 = 0
-i = 1
-for i in range(1, x + 1) :
-    sum_1 = sum_1 + i ** 3
-print(sum)
+for x in range(1, x + 1) :
+    sum_1 += x ** 3
+print(sum_1)
+
 
 """
 
-#second variant
 x = int(input())
 sum_1 = 0
 i = 1
@@ -17,3 +16,4 @@ while i <= x :
     sum_1 += i ** 3
     i += 1
 print(sum_1)
+"""
--- src/eugenia_hw3/eugenia_hw3_5.py
+++ src/eugenia_hw3/eugenia_hw3_5.py
@@ -1,17 +1,16 @@
+import random
 a = int(input("enter a: "))
 b = int(input("enter b: "))
-import random
 
 number = random.randint(a, b)
 number_1 = int(input("enter a number: "))
-
 while number != number_1 :
     if number == number_1 :
         break
     elif number < number_1 :
         print("попробуй меньшее число")
-        number_1 = int(input())
     elif number > number_1 :
         print("попробуй большее число")
-        number_1 = int(input())
+    number_1 = int(input("enter a number: "))
 print("ты угадал")
+
