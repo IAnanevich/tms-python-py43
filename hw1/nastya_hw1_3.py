e89ed32e92b0cf1c9d6168d2afc96ae3d2575e6d
Анастасия Тилигузова <jkfrjkvkd@gmail.com>
2023-08-06 18:02:40 +0300

    ÐHW3: Nastya Tiliguzova

--- hw1/nastya_hw1_3.py
+++
@@ -1,4 +0,0 @@
-# 3 |x|-|y|/1+|x*y|
-x = int(input('Ввод x: '))  # текст в input
-y = int(input('Ввод y: '))
-print(f'|{x}|-|{y}|/1+|{x}*{y}|= {abs(x)-abs(y)/1+abs(x*y)}')
--- hw2/nastya_hw2_1.py
+++
@@ -1,3 +0,0 @@
-# Есть строка 'Hello World'. Сделать из нее 'dlroW olleH' (сделать реверсивную строку)
-
-print('Hello World'[::-1])
