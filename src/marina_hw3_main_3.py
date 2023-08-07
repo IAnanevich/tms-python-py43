d239340539021e0c32b0c2bb07b91788d3d0bbcb
Marina_Pudova <marinpudova@gmail.com>
2023-08-08 00:08:01 +0300

    Fix HW3.(+main_3)

--- src/marina_hw3_main_3.py
+++ src/marina_hw3_main_3.py
@@ -4,15 +4,13 @@ from random import randint
 
 number = randint(1, 100)
 # print(number)  # чтобы знать. что угадать)
-guess_number = int(input('Please, guess target number (from 1 to 100: '))
 
 while True:
+    guess_number = int(input('Please, guess target number (from 1 to 100): '))
     if guess_number > number:
         print(f'Your number {guess_number} is bigger than target, try again :)')
-        guess_number = int(input('Please, guess target number: '))
     elif guess_number < number:
         print(f'Your number {guess_number} is less than target, try again :)')
-        guess_number = int(input('Please, guess target number: '))
     else:
         print(f'!Congratulations! \nYour number {guess_number} is right :)')
         break
