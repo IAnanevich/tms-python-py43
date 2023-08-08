b5a935e86dcd7f0611bac388c659c62a3d8ec33c
Roman <redkoroma413@gmail.com>
2023-08-08 18:52:13 +0300

    add hw3new

--- hw3/hw3_s_1.py
+++ hw3/hw3_s_1.py
@@ -3,20 +3,16 @@
 Рассмотреть все случаи дискриминанта.'''
 
 import math
-
 a = float(input("Введите коэффициент a: "))
 b = float(input("Введите коэффициент b: "))
 c = float(input("Введите коэффициент c: "))
 discriminant = b ** 2 - 4 * a * c
 if discriminant > 0:
-    x1 = (-b + math.sqrt(discriminant)) / (2 * a)
-    x2 = (-b - math.sqrt(discriminant)) / (2 * a)
     print("Уравнение имеет два корня:")
-    print("x1 =", x1)
-    print("x2 =", x2)
+    print("x1 =", (-b + math.sqrt(discriminant)) / (2 * a))
+    print("x2 =", (-b - math.sqrt(discriminant)) / (2 * a))
 elif discriminant == 0:
-    x = -b / (2 * a)
     print("Уравнение имеет один корень:")
-    print("x =", x)
+    print("x =", -b / (2 * a))
 else:
     print("Нет корней")
--- hw3/hw3_s_4.py
+++ hw3/hw3_s_4.py
@@ -5,6 +5,6 @@
 dictionary = {'test': 'test_value', 'europe': 'eur', 'dollar': 'usd', 'ruble': 'rub'}
 new_dictionary = {}
 for key in dictionary.keys():
-    new_key = key + str(len(key))
+    new_key = f"{key}{len(key)}"
     new_dictionary[new_key] = dictionary[key]
 print(new_dictionary)
