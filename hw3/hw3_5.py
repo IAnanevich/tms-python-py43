217cde7db35a84759488a49f091f0cd1224cf1c6
Roman <redkoroma413@gmail.com>
2023-08-08 18:28:36 +0300

    add clw

--- hw3/hw3_5.py
+++ hw3/hw3_5.py
@@ -1,15 +1,16 @@
 import random
+
 a = int(input("Введите нижнюю границу : "))
 b = int(input("Введите верхнюю границу: "))
 
 number = random.randint(a, b)
 number_1 = int(input("Введите ваше число: "))
-while number != number_1 :
-    if number == number_1 :
+while number != number_1:
+    if number == number_1:
         break
-    elif number < number_1 :
+    elif number < number_1:
         print("Ваше число больше загаданного")
-    elif number > number_1 :
+    elif number > number_1:
         print("Ваше число меньше загаданного")
     number_1 = int(input("Введите ваше число: "))
 print("Вы угадали число!")
