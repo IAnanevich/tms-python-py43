7c5335742534db7682b0f3ed543c9e112e49d163
EkMakarenko <makarenkoek@yandex.ru>
2023-08-08 18:52:07 +0300

    Fix new hw3

--- src/HW_3/katya_3_6.py
+++ src/HW_3/katya_3_6.py
@@ -1,9 +1,16 @@
-n = 2
-k = 2
-list_f = [1, 1]
+# n = 2
+# k = 2
+# list_f = [1, 1]
+#
+# while n <= 15:
+#     list_f.append(k)
+#     k = list_f[n] + list_f[n - 1]
+#     n += 1
+# print(f' список чисел фибоначчи {list_f}')
 
-while n <= 15:
-    list_f.append(k)
+list_f = [1, 1]
+for n in range(1, 15):
     k = list_f[n] + list_f[n - 1]
-    n += 1
-print(f' список чисел фиббоначи {list_f}')
+    list_f.append(k)
+
+print(f' список чисел фибоначчи {list_f}')
