d8cff6e8e557cb19b87a652f849ecd47e95f45df
Marina_Pudova <marinpudova@gmail.com>
2023-08-08 23:05:13 +0300

    Fix HW3(3_3,3_6)

--- src/marina_hw3_4.py
+++ src/marina_hw3_4.py
@@ -8,8 +8,12 @@
 
 dict_currency = {'test': 'test_value', 'europe': 'eur', 'dollar': 'usd', 'ruble': 'rub'}
 
-for key_c in tuple(dict_currency.keys()):
-    dict_currency[f'{key_c}{str(len(key_c))}'] = dict_currency[key_c]
-    del dict_currency[key_c]
+# for key_c in tuple(dict_currency.keys()):
+#     dict_currency[f'{key_c}{str(len(key_c))}'] = dict_currency[key_c]
+#     del dict_currency[key_c]
+#
+# print(dict_currency)
 
-print(dict_currency)
+new_dict_currency = {f'{key}{len(key)}': value for key, value in dict_currency.items()}
+
+print(new_dict_currency)
--- src/marina_hw3_6.py
+++ src/marina_hw3_6.py
@@ -5,12 +5,22 @@
 
 # n = 15
 n = int(input("Enter length Fibonacci list: "))
-fib_list = list(range(n))
+# fib_list = list(range(n))
+fib_list = [1]  # полагаем, что введут нормальное число
+a = 0
+b = 1
 
-for i in range(n):
-    if i < 2:
-        fib_list[i] = 1
-    else:
-        fib_list[i] = fib_list[i - 1] + fib_list[i - 2]
+# for i in range(n):
+#     if i < 2:
+#         fib_list[i] = 1
+#     else:
+#         fib_list[i] = fib_list[i - 1] + fib_list[i - 2]
+#
+# print(*fib_list, sep=', ')
+
+while n - 1:
+    a, b = b, a + b
+    fib_list.append(b)
+    n -= 1
 
 print(*fib_list, sep=', ')
