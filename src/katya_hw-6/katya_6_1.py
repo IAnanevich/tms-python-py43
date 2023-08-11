aea17d0f778f2078b728fde6a3149071bacf0b67
EkMakarenko <makarenkoek@yandex.ru>
2023-08-11 15:42:45 +0300

    Add hw6

---
+++ src/katya_hw-6/katya_6_1.py
@@ -0,0 +1,12 @@
+# Сделать фунцкию деление чисел и обернутьдекоратором,
+# который проверял бы деление на ноль, и отказывал в
+# работе пользователю
+
+def div(elem1: float, elem2: float) -> float:
+    """
+    Calculating the dividing
+    :param elem1: first number
+    :param elem2: second number
+    :return: the dividing the first number by the second numbers
+    """
+    return elem1/elem2
