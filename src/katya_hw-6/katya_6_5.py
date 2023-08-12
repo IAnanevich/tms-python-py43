74249059c66c6f6729df88883a1a850a93516bbf
EkMakarenko <makarenkoek@yandex.ru>
2023-08-12 22:22:11 +0300

    hw6

--- src/katya_hw-6/katya_6_5.py
+++ src/katya_hw-6/katya_6_5.py
@@ -2,6 +2,6 @@
 
 numbers = int(input('Введите число: '))
 
-even_or_odd = lambda x: print('четное') if x / 2 == 0 else print('нечетное')
+even_or_odd = lambda x: print('четное') if x % 2 == 0 else print('нечетное')
 
 even_or_odd(numbers)
