19e88e64c55cad4591650671a82e6fb4fcf40528
КшсRichard Kolomeichuk <rickkolomeichuk@gmail.com>
2023-08-13 02:22:14 +0300

    last try flake 8

--- src/Rich_Hw_5/Rich_5_4.py
+++ src/Rich_Hw_5/Rich_5_4.py
@@ -1,10 +1,10 @@
 import time
 
-
 def get_current_time():
     time.sleep(1)
     current_time = time.strftime('%H:%M:%S')
     return current_time
+
 
 n = int(input("Введите количество элементов в списке: "))
 time_list = [get_current_time() for _ in range(n)]
