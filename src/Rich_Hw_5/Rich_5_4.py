493e82b356c9ad6d08780ac8e4bdd9f2f34ff08d
КшсRichard Kolomeichuk <rickkolomeichuk@gmail.com>
2023-08-13 02:17:49 +0300

    flake8

--- src/Rich_Hw_5/Rich_5_4.py
+++ src/Rich_Hw_5/Rich_5_4.py
@@ -5,6 +5,7 @@ def get_current_time():
     current_time = time.strftime('%H:%M:%S')
     return current_time
 
+
 n = int(input("Введите количество элементов в списке: "))
 time_list = [get_current_time() for _ in range(n)]
 print("Список времен с задержкой:")
