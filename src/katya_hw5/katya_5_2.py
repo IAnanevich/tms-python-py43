0ee5df1ac47fc49611c2aa6da22bfc4584f8b52c
EkMakarenko <makarenkoek@yandex.ru>
2023-08-14 14:57:21 +0300

    fix hw5

--- src/katya_hw5/katya_5_2.py
+++ src/katya_hw5/katya_5_2.py
@@ -9,15 +9,18 @@ def fact(a: int) -> int:
     """
     if a > 1:
         return a * fact(a-1)
+    return 1
+
+
+while True:
+    number_fact = input('Введите число для подсчета факторияла: ')
+    if number_fact.isdigit():
+        number_fact_n = int(number_fact)
+        if number_fact_n == 1:
+            print('Факториал числа 1 равен 1')
+        elif number_fact_n > 0:
+            print(f'Факториял числа {number_fact_n} равен {fact(number_fact_n)}')
+        else:
+            print('Введите число больше О')
     else:
-        return 1
-
-
-number_fact = int(input('ВВедите число для подсчета факторияла'))
-
-if number_fact == 1:
-    print('Факториал числа 1 равен 1')
-elif number_fact > 0:
-    print(f'Факториял числа {number_fact} равен {fact(number_fact)}')
-else:
-    print('Введите число больше О')
+        print('Введите число')
