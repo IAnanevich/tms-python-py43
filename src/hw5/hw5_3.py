feaf5edc46457b25460da377385b0287e770a65d
Oleg Pravlutski <pravlutski@gmail.com>
2023-08-15 22:12:05 +0300

    HW6: Oleg Pravlutski

--- src/hw5/hw5_3.py
+++
@@ -1,9 +0,0 @@
-# сколько раз встречается числа в списке
-list_numbers = [1,3,5,2,2,3,4,2,3,1,4,1]
-
-list_sum = {}
-for number in list_numbers:
-    if list_sum.get(number) is None:
-        list_sum[number] = list_numbers.count(number)
-
-print(list_sum)
