c39418c7ebb757a91a60ab4f38b04a6cb3266eeb
Edgar Dralov <edgar.dralov@gmail.com>
2023-08-16 10:16:50 +0300

    HW7: Edgar Dralov

--- src/edgar_hw7/edgar_hw7_1.py
+++ src/edgar_hw7/edgar_hw7_1.py
@@ -3,3 +3,14 @@
 # (результаты всех преобразование выводить на экран).
 # =================================================================================================================
 
+original_bytes = b'r\xc3\xa9sum\xc3\xa9'
+
+# Conversion to byte form in "Latin1" encoding
+
+latin1_bytes = original_bytes.decode("utf-8").encode("latin1")
+print("Latin1 bytes: ", latin1_bytes)
+
+# Decoding to string from byte view in "Latin1" encoding
+
+decoded_string = latin1_bytes.decode("latin1")
+print("Decoded string: ", decoded_string)
