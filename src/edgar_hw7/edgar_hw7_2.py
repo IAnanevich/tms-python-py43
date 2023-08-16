4ebaf3cdd6753c5ea3d30ec10516be518269b01f
Edgar Dralov <edgar.dralov@gmail.com>
2023-08-16 11:20:17 +0300

    HW7: Edgar Dralov

--- src/edgar_hw7/edgar_hw7_2.py
+++ src/edgar_hw7/edgar_hw7_2.py
@@ -2,3 +2,18 @@
 # Создать файл и записать в него первые 2 строки и закрыть файл.
 # Затем открыть файл на редактирование и дозаписать оставшиеся 2 строки.
 # В итоговом файле должны быть 4 строки, каждая из которых должна начинаться с новой строки.
+
+lines = []
+for i in range(4):
+    line = input(f"Enter line {i + 1}: ")
+    lines.append(line)
+
+# Writing the first 2 lines to a file
+with open('output.txt', 'w') as file:
+    for line in lines[:2]:
+        file.write(line + '\n')
+
+# Finish writing the remaining 2 lines to the file
+with open('output.txt', 'a') as file:
+    for line in lines[2:]:
+        file.write(line + '\n')
