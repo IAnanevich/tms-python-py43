2f8d4cd12fcbcdac82c7c869c2a614e26e64f15c
КшсRichard Kolomeichuk <rickkolomeichuk@gmail.com>
2023-08-21 22:51:19 +0300

    hw

---
+++ src/lesson_8/RichHW8/Rich_8_1.py
@@ -0,0 +1,24 @@
+# Создать родительский класс Auto, у которого есть Атрибуты: brand, age, color, mark, weight.
+# Методы move, bithday, stop.
+# Методы  "move" и "stop" выводят сообщения на экран "move" и "stop", а birthday увеличивает атрибут age на 1.
+# Атрибуты brand, age и mark обязательные при объявлении объекта.
+
+# class ****:
+#     def __init__(self, parameter1, parameter2):
+#         self.parameter1 = parameter1
+#         self.parameter2 = parameter2
+#
+#     def method1(self):
+#         # Код метода 1
+#         pass
+#
+#     def method2(self):
+#         # Код метода 2
+#         pass
+#
+# # Создание объекта класса
+# obj = MyClass(parameter1_value, parameter2_value)
+#
+# # Вызов методов объекта
+# obj.method1()
+# obj.method2()
