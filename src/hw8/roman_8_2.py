94adc6c8d60f47127489039d2e4c489908fafa18
Roman <redkoroma413@gmail.com>
2023-08-23 00:36:03 +0300

    hw8

--- src/hw8/roman_8_2.py
+++ src/hw8/roman_8_2.py
@@ -4,7 +4,7 @@
 
 
 class Auto:
-    def __init__(self, brand: str, age: int, mark: str, color: str="", weight: float=0.0):
+    def __init__(self, brand: str, age: int, mark: str, color: str = "", weight: float = 0.0):
         self.brand = brand
         self.age = age
         self.mark = mark
@@ -26,7 +26,7 @@ car2 = Auto(brand="Nissan", age=20, mark="Primera")
 car3 = Auto(brand="Lada", age=50, mark="2101", color="Yellow")
 
 car1.move()  # Выводит "move"
-print(car1.mark) #Выводит Challenger
+print(car1.mark)  # Выводит Challenger
 print(car1.age)  # Выводит 3
 car1.birthday()
 print(car1.age)  # Выводит 4
@@ -35,8 +35,8 @@ car1.stop()  # Выводит "stop"
 print()
 
 car2.move()  # Выводит "move"
-print(car2.brand,car2.mark) #Выводит Nissan Primera
-print(car2.age) # Выводит 3
+print(car2.brand, car2.mark)  # Выводит Nissan Primera
+print(car2.age)  # Выводит 3
 car2.birthday()
-print(car2.age) # Выводит 21
-car2.stop() # Выводит "stop"
+print(car2.age)  # Выводит 21
+car2.stop()  # Выводит "stop"
