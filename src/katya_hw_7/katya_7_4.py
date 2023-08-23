f9aec8c200385aa105f2acb443ab62bed0ccd066
EkMakarenko <makarenkoek@yandex.ru>
2023-08-23 15:50:05 +0300

    Fix hw7

--- src/katya_hw_7/katya_7_4.py
+++ src/katya_hw_7/katya_7_4.py
@@ -4,10 +4,10 @@
 import csv
 import json
 
-with open('katya_7_3.json', 'r') as new_file_json:
+with open("katya_7_3.json", "r") as new_file_json:
     file_katya = json.loads(new_file_json.read())
-    with open('katya_7_4.csv', 'w') as new_file_csw:
-        writer = csv.writer(new_file_csw, delimiter=';')
-        writer.writerow(['id', 'name', 'years', 'phone', file_katya])
-        for line in new_file_json:
-            new_file_csw.write(line)
+    with open("katya_7_4.csv", "w") as new_file_csw:
+        writer = csv.DictWriter(new_file_csw, fieldnames=["id", "name", "years", "phone"])
+        writer.writeheader()
+        for key, value in file_katya.items():
+            writer.writerow({"id": key, "name": value[0], "years": value[1], "phone": "+375 29"})
