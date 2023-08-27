b82bf28995f9b5a8c24357339431281c6313f3c0
EkMakarenko <makarenkoek@yandex.ru>
2023-08-27 22:53:41 +0300

    Fix tasks

--- src/additional_tasks/task_18/task_18.py
+++ src/additional_tasks/task_18/task_18.py
@@ -1,2 +1,35 @@
 # Создать матрицу случайных чисел и сохранить ее в json файл.
 # После прочесть ее, обнулить все четные элементы и сохранить в другой файл
+import json
+import numpy as np
+
+def matrix_def(matr):
+    for i in range(len(matr)):
+        for j in range(len(matr[i])):
+            if matr[i][j] % 2:
+                matr[i][j] = 0
+    return matr
+
+a = int(input('Введите количество столбцов: '))
+b = int(input('Введите количество строк: '))
+
+matrix_18 = np.random.randint(1, 100, (a, b))
+
+with open("task_18_1.json", "w") as new_file:
+    new_file.write(json.dumps(matrix_18))
+
+with open("task_18.json", "r") as new_file:
+    matrix_18_n = json.loads(new_file.read())
+
+matrix_18_2 = matrix_def(matrix_18_n)
+
+with open("task_18_2.json", "w") as new_file:
+    new_file.write(json.dumps(matrix_18_2))
+
+
+
+
+
+
+
+
