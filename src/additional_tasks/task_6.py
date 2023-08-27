c74e0a842231190aba86e3335a55946e11e4b258
EkMakarenko <makarenkoek@yandex.ru>
2023-08-27 22:04:59 +0300

    Fix tasks

--- src/additional_tasks/task_6.py
+++ src/additional_tasks/task_6.py
@@ -1,2 +1,19 @@
 # Дана целочисленная квадратная матрица. Найти в каждой строке наибольший
 # элемент и поменять его местами с элементом главной диагонали.
+
+
+matrix_6 = [
+    [-11, 1, 20],
+    [19, -1, 10],
+    [18, 18, -1],
+]
+
+for i in range(len(matrix_6)):
+    max_num = max(matrix_6[i])
+    for j in range(len(matrix_6[i])):
+        if matrix_6[i][j] == max_num:
+            matrix_6[i][j] = matrix_6[i][i]
+            matrix_6[i][i] = max_num
+
+for i in matrix_6:
+    print(i)
