2ddb9e89b1e0c69b723201a6a279dd72a49619d5
MaxGvozd <champion.91@mail.ru>
2023-08-29 15:25:11 +0300

    HW7: MaximGvozd fix 7_3, 7_4

---
+++ src/lesson_4/4.py
@@ -0,0 +1,42 @@
+from typing import Union, Any
+
+# шаблон
+# def имя_функции(параметр_1, параметр_2, ...):
+#     код
+#     return переменная(ые) / (ничего) / None / без return
+
+
+def summa(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+    """
+    Add a to b
+    :param a: first number
+    :param b: second number
+    :return: sum of 2 numbers
+    """
+    return a + b
+
+
+def mult(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+    """
+
+    :param a:
+    :param b:
+    :return:
+    """
+    return a * b
+
+
+def increase_list(numbers: Any) -> list[int]:
+    """
+
+    :param numbers:
+    :return:
+    """
+    new_numbers = []
+    for i in numbers:
+        new_numbers.append(i + 1)
+
+    return new_numbers
+
+
+print(mult(2, 3.2))
