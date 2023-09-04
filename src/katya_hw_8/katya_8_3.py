7924fde1a4cd4a93c48f6bd6ce11656352943798
EkMakarenko <makarenkoek@yandex.ru>
2023-09-05 00:33:24 +0300

    Fix hw8

--- src/katya_hw_8/katya_8_3.py
+++ src/katya_hw_8/katya_8_3.py
@@ -1,13 +1,27 @@
 class RealString:
-    def __init__(self, str_f):
+    def __init__(self, str_f: str) -> None:
+        """
+
+        :param str_f:
+        """
         self.str_f = str_f
 
-    def __lt__(self, other):
+    def __lt__(self, other: str) -> bool:
+        """
+
+        :param other:
+        :return: len(self.str_f) < len(other.str_f)
+        """
         if not isinstance(other, RealString):
             other = RealString(other)
         return len(self.str_f) < len(other.str_f)
 
-    def __gt__(self, other):
+    def __gt__(self, other: str) -> bool:
+        """
+
+        :param other:
+        :return: len(self.str_f) > len(other.str_f)
+        """
         if not isinstance(other, RealString):
             other = RealString(other)
         return len(self.str_f) > len(other.str_f)
