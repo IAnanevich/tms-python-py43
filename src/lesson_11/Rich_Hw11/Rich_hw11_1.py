b0f602a9ceffec95e2fc089d26a8505db0cbc11d
Richard Kolomeichuk <Rickkolomeichuk@gmail.com>
2023-09-11 22:13:36 +0300

    hw

---
+++ src/lesson_11/Rich_Hw11/Rich_hw11_1.py
@@ -0,0 +1,26 @@
+# Создать генератор геометрической прогрессии
+
+def geometric_progression(param_a, param_b, param_c):
+    """
+    :param param_a: one num
+    :param param_b: koef progression
+    :param param_c: generic element
+    :return:
+    """
+    current = float(param_a)
+    generated = 0
+
+    while generated < int(param_b):
+        yield current
+        current *= float(param_c)
+        generated += 1
+
+
+param_a = input("Num: ")
+param_b = input("koefficent progression: ")
+param_c = input("Element: ")
+
+progression = geometric_progression(param_a, param_b, param_c)
+
+for element in progression:
+    print(element)
