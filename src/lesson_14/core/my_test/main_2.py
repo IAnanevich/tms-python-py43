4be2e2ecb105fb67be9a49182640583ad0e6b3c6
iananevich <ivanshahter71@gmail.com>
2023-09-15 15:59:23 +0300

    add lesson 14

---
+++ src/lesson_14/core/my_test/main_2.py
@@ -0,0 +1,14 @@
+# import numpy
+
+# import numpy as np
+
+# from numpy import (
+#       matrix,
+#       array,
+# )
+
+# from numpy import matrix as mt
+
+# np.matrix() = numpy.matrix()
+# np.array() = numpy.array()
+# matrix() = mt()
