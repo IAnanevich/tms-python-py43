dbfa31b9fb04ce53e2cc4cf64df98b57ce97d270
VicVV <vic.v.v@mail.ru>
2023-09-26 23:18:53 +0300

    HW6: Victor Vlasov

--- src/VicVV_hw_06-3.py
+++ src/VicVV_hw_06-3.py
@@ -1,6 +1,6 @@
 # При помощи функции filter() из котрежа слов отфильтровать только те, которые являются
 # полиндромами (читаются одинаково в обе стороны)
 
-words = ("cat", "rewire", "level", "book", "stats", "list", "АННА", "шалаш", "закаЗ")
-pal_w = tuple(filter(lambda words: words == words[::-1], words))
+words_ = ("cat", "rewire", "level", "book", "stats", "list", "АННА", "шалаш", "закаЗ")
+pal_w = tuple(filter(lambda words: words == words[::-1], words_))
 print("перевёртыши: ", pal_w)
