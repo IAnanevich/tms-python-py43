a87704b83592b10b7e29835d57f076ed2cdec5fa
EkMakarenko <makarenkoek@yandex.ru>
2024-01-25 00:26:11 +0300

    fix hw_35

--- src/katya_hw_35/hw_35_generator.py
+++ src/katya_hw_35/hw_35_generator.py
@@ -4,6 +4,7 @@ from time import sleep
 def fib(n):
     a, b = 1, 1
     for i in range(n):
+        # sleep(0.5)
         yield a
         a, b = b, a + b
 
@@ -12,4 +13,4 @@ gen = fib(10)
 for numbers in gen:
     print(numbers)
 
-print(list(fib(10)))
+print(list(fib(20)))
